9cd0429fb5440b659d5695863b0a0261d8f310c4
Shaban Mohammed Hidaayah <shabanmohammedhidaayah4@gmail.com>
2025-08-13 19:31:33 +0100

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,7 +1,6 @@
--- task_4.sql
-
--- Print the full description of the books table
-SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM information_schema.columns
-WHERE TABLE_NAME = 'books' AND TABLE_SCHEMA = DATABASE();
-
+USE alx_book_store;
+SHOW FULL COLUMNS FROM alx_book_store.Books;
+SELECT COLUMN_NAME, COLUMN_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+AND TABLE_NAME = 'Books';
