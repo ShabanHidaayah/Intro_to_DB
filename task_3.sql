66e9144b6f3dbe281449ed667f23677403c47573
Shaban Mohammed Hidaayah <shabanmohammedhidaayah4@gmail.com>
2025-08-13 19:27:01 +0100

    Update task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -1,6 +1,6 @@
+
 -- task_3.sql
 
 -- List all tables in the database
-SELECT TABLE_NAME 
-FROM information_schema.TABLES 
-WHERE TABLE_SCHEMA = 'alx_book_store';
+SHOW TABLES FROM alx_book_store;
+
